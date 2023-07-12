1e88825d1d76d2dab5d9be0a5b53d1a00480f3bb
Sienna <135257695+siennaphia@users.noreply.github.com>
2023-07-12 12:20:28 -0400

    updates class table & readme

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # FAUmyAdvisor
+
+## Academic Record Management and Career Matching
+
+This project aims to create a web application that assists users in managing their academic records, exploring available courses, providing supplemental information about classes, and matching their acquired skills with suitable careers. The application allows users to log in, add completed courses, search for classes, view available courses based on completed classes, access additional information about specific classes, and obtain career recommendations based on their skill proficiencies.
+
+## Features
+
+- **Account Login**: Users can log in to the application using their username/email and password. If the login is successful, the main page is displayed. If the login fails, an error message is shown.
+
+- **Managing Academic Records**: Users can add completed courses to their account. They can select the term in which the courses were taken and choose from a list of courses offered during that semester. Selected courses are added to the user's academic record.
+
+- **Searching for Classes**: Users can search for classes by entering a search string. The system queries for a list of available classes in the user's major and displays the matching classes based on the search string.
+
+- **Viewing Available Courses**: Users can explore future semesters and view available courses. They can select a semester from a dropdown menu, and the system retrieves a list of courses available in the chosen semester. The system also checks prerequisite requirements and displays courses that meet those requirements.
+
+- **Viewing Supplemental Information**: Users can select a class and view additional information. The system retrieves course descriptions and professor ratings from external sources, allowing users to see the course description and professor ratings.
+
+- **Matching Acquired Skills with Careers**: The system analyzes the courses taken by the user and determines the acquired skills. Based on the acquired skills, the system suggests suitable careers. The system also provides information on how the user's skill proficiencies match up to their ideal career.
+
+- **Editing Semester Stored in Database**: Users can edit the contents of a semester stored in the database.
+
+## System Architecture
+
+The project utilizes a Firebase database  for storing course information, user data, and career requirements. The database structure is as follows:
+
+```
+{
+  "careers": {
+    // Career information and required skills
+  },
+  "classes": {
+    "core": {
+      // Core class information
+    },
+    "elective": {
+      // Elective class information
+    }
+  },
+  "currentUser": {
+    // Current user's data (auth, classes taken, skills)
+  },
+  "users": {
+    // User information and data
+  }
+}
+```
+
+Please refer to the project code and documentation for detailed implementation and usage instructions.
+
+## Contributors
+
+- Sienna Gaita-Monjaraz
+- Maximo Mejia
+- Owen Levine
+- Cassidy Wilson
--- public/app.js
+++ public/app.js
@@ -12,96 +12,132 @@ var firebaseConfig = {
   };
 
   firebase.initializeApp(firebaseConfig);
-  var database = firebase.database();
-  
-  let classes = {};
-  let currentUser = {
-    "classesTaken": [],
-    "acquiredSkills": {}
-  };
-  
-    // Fetch classes from Firebase
-    database.ref('/classes').once('value', snapshot => {
-        classes = snapshot.val();
-        console.log(classes);  // Add this line
-        // Fetch User 1 (for now) from Firebase
-          // Fetch User 1 (for now) from Firebase
-  database.ref('/users/user1').once('value', snapshot => {
-    currentUser = snapshot.val();
-    
-    // Check if currentUser.classesTaken is an array, if not initialize it as an empty array
-    if (!Array.isArray(currentUser.classesTaken)) {
-      console.log('classesTaken is not an array, initializing it as an empty array');
-      currentUser.classesTaken = [];
-    }
-    
-    // Check if currentUser.acquiredSkills is an object, if not initialize it as an empty object
-    if (typeof currentUser.acquiredSkills !== 'object' || currentUser.acquiredSkills === null) {
-      console.log('acquiredSkills is not an object, initializing it as an empty object');
-      currentUser.acquiredSkills = {};
-    }
+var database = firebase.database();
 
-    createCheckboxes();
-  });
+let classes = {};
+let currentUser = {
+  firstName: "",
+  lastName: "",
+  classesTaken: [],
+  acquiredSkills: {}
+};
 
-      });
-    
-  
-  // Save currentUser to Firebase when 'Save' button is clicked
-  document.getElementById('saveButton').addEventListener('click', () => {
-    database.ref('/users/user1').set(currentUser); // Update the user data in the Firebase
+// Fetch classes from Firebase
+database.ref('/classes').once('value', snapshot => {
+  classes = snapshot.val();
+  createCheckboxes();
+});
+
+// Fetch User 1 (for now) from Firebase
+database.ref('/users/user1').once('value', snapshot => {
+  currentUser = snapshot.val() || currentUser;
+  createCheckboxes();
+});
+
+// Save currentUser to Firebase when 'Save' button is clicked
+document.getElementById('saveButton').addEventListener('click', () => {
+  // Update the user data in the Firebase
+  database.ref('/users/user1').set(currentUser, error => {
+    if (error) {
+      console.log('Error while saving data:', error);
+    } else {
+      console.log('Data saved successfully!');
+    }
   });
-  
-  function createCheckboxes() {
-    const classesContainer = document.getElementById('classesContainer');
-    for (const classType in classes) {
-      for(const classId in classes[classType]){
-        const classItem = document.createElement('div');
-        const checkbox = document.createElement('input');
-        checkbox.type = 'checkbox';
-        checkbox.id = classId;
-        checkbox.value = classId;
-        checkbox.addEventListener('change', function(e) {
-          const classData = classes[classId];
-          if (e.target.checked) {
-            currentUser.classesTaken.push(classId);
-            const skills = classData.skillsTaught;
-            for (const skill in skills) {
-              if (currentUser.acquiredSkills[skill]) {
-                currentUser.acquiredSkills[skill] += skills[skill];
-              } else {
-                currentUser.acquiredSkills[skill] = skills[skill];
-              }
-            }
-          } else {
-            const index = currentUser.classesTaken.indexOf(classId);
-            if (index !== -1) {
-              currentUser.classesTaken.splice(index, 1);
-            }
-            const skills = classData.skillsTaught;
-            for (const skill in skills) {
-              currentUser.acquiredSkills[skill] -= skills[skill];
-              if (currentUser.acquiredSkills[skill] <= 0) {
-                delete currentUser.acquiredSkills[skill];
-              }
-            }
-          }
-        });
-    
-        // Check if the classId is in the currentUser's classesTaken array
-        // If it is, mark this checkbox as checked
-        if (currentUser.classesTaken.includes(classId)) {
-          checkbox.checked = true;
-        }
-    
-        const label = document.createElement('label');
-        label.htmlFor = classId;
-        label.textContent = classes[classType][classId].name;
-    
-        classItem.appendChild(checkbox);
-        classItem.appendChild(label);
-        classesContainer.appendChild(classItem);
+});
+
+// Update currentUser object with the selected classes
+function updateSelectedClasses() {
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  currentUser.classesTaken = Array.from(checkboxes)
+    .filter(checkbox => checkbox.checked)
+    .map(checkbox => checkbox.value);
+}
+
+// Update currentUser object with the entered first and last name
+function updateUserName() {
+  currentUser.firstName = document.getElementById('firstNameInput').value;
+  currentUser.lastName = document.getElementById('lastNameInput').value;
+}
+
+// Create checkboxes for classes
+function createCheckboxes() {
+  if (!classes || Object.keys(classes).length === 0 || !currentUser) {
+    return;
+  }
+
+  const classesContainer = document.getElementById('classesContainer');
+  classesContainer.innerHTML = '';
+
+  const classesTable = document.createElement('table');
+  classesTable.classList.add('classes-table');
+
+  let rowCounter = 1;
+  let cellCounter = 1;
+  let currentRow = document.createElement('tr');
+  currentRow.id = `row${rowCounter}`;
+  classesTable.appendChild(currentRow);
+
+  for (const classType in classes) {
+    for (const classId in classes[classType]) {
+      const classData = classes[classType][classId];
+
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.id = classId;
+      checkbox.value = classId;
+      checkbox.addEventListener('change', updateSelectedClasses);
+
+      if (currentUser.classesTaken.includes(classId)) {
+        checkbox.checked = true;
+      }
+
+      const label = document.createElement('label');
+      label.htmlFor = classId;
+      label.textContent = classData.name;
+
+      const cell = document.createElement('td');
+      cell.appendChild(checkbox);
+      cell.appendChild(label);
+
+      currentRow.appendChild(cell);
+      cellCounter++;
+
+      // Move to the next row if we have reached the maximum number of columns
+      if (cellCounter > 5) {
+        rowCounter++;
+        cellCounter = 1;
+        currentRow = document.createElement('tr');
+        currentRow.id = `row${rowCounter}`;
+        classesTable.appendChild(currentRow);
       }
     }
   }
-  
+
+  // Remove empty rows
+  while (rowCounter <= 5) {
+    const emptyRow = document.getElementById(`row${rowCounter}`);
+    if (emptyRow) {
+      emptyRow.parentNode.removeChild(emptyRow);
+    }
+    rowCounter++;
+  }
+
+  classesContainer.appendChild(classesTable);
+}
+
+// Initialize the user name inputs
+function initializeUserNameInputs() {
+  const firstNameInput = document.getElementById('firstNameInput');
+  const lastNameInput = document.getElementById('lastNameInput');
+
+  firstNameInput.value = currentUser.firstName || '';
+  lastNameInput.value = currentUser.lastName || '';
+
+  firstNameInput.addEventListener('input', updateUserName);
+  lastNameInput.addEventListener('input', updateUserName);
+}
+
+// Call the initialization functions
+createCheckboxes();
+initializeUserNameInputs();
