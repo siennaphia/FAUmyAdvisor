35f7cbee59f0429eda63082024b5d9351155a295
Maximo Mejia <mejiam2017@fau.edu>
2023-07-18 18:46:30 -0400

    Added to registration page

--- public/registration.js
+++ public/registration.js
@@ -35,29 +35,67 @@ database.ref('/classes/elective').once('value', function(electiveSnapshot) {
     elective = electiveSnapshot.val();
 });
 //Fetch classes taken by the current user from Firebase
-database.ref('/currentUser/classesTaken').once('value', function(classesTakenSnapshot) {
+database.ref('/users/UID1/classesTaken').once('value', function(classesTakenSnapshot) {
     classesTaken = classesTakenSnapshot.val();
 });
 
-
-
 function populateClasses(){
-
+    
     var classChoiceContainer = document.getElementById("classChoiceContainer");
     var classChoice = document.getElementById("classChoice");
-
-    for (var classId in nextSemesterClass) {
-        if (nextSemesterClass.hasOwnProperty(classId)) {
-            var classObj = nextSemesterClass[classId];
-            console.log(classObj);
-            var listItem = document.createElement("li");
-                listItem.textContent = classId + " - " + classObj["name"];
-                listItem.addEventListener("click", function() {
-                    classInfo(this.innerHTML); // Pass the innerHTML of the clicked element
-                  });
-                classChoice.appendChild(listItem);
+    var dropdownSemester = document.getElementById("semester");
+    var dropdownReason = document.getElementById("reason");
+    
+    
+    var selectedSemester = dropdownSemester.value;
+    var selectedReason = dropdownReason.value;
+    
+    // Clear the classChoice list
+    classChoice.innerHTML = "";
+        // Filter and display classes offered in Fall semester
+        if (selectedSemester == "Fall") {
+            for (var classId in nextSemesterClass) {
+                
+                if (nextSemesterClass.hasOwnProperty(classId)) {        
+                    var classObj = nextSemesterClass[classId];
+                    console.log(classObj["classReason"]);
+                    console.log("==========")
+                    console.log(selectedReason);
+                    
+                    if (classObj["semesterAvailable"] === "Fall" && classObj["classReason"] === selectedReason) {
+                        console.log("==========ASDADADASDASD=========")
+                        var listItem = document.createElement("li");
+                        listItem.textContent = classId + " - " + classObj.name;
+                        listItem.addEventListener("click", function() {
+                            classInfo(this.innerHTML);
+                        });
+                        classChoice.appendChild(listItem);
+                    }
+                }
+            }
         }
-    }
+        
+        // Filter and display classes offered in Spring semester
+        else if (selectedSemester == "Spring") {
+            for (var classId in nextSemesterClass) {
+                if (nextSemesterClass.hasOwnProperty(classId)) {        
+                    var classObj = nextSemesterClass[classId];
+                    console.log(classObj["classReason"]);
+                    console.log("==========")
+                    console.log(selectedReason);
+                    
+                    if (classObj["semesterAvailable"] === "Spring" && classObj["classReason"] === selectedReason) {
+                        console.log("==========ASDADADASDASD=========")
+                        var listItem = document.createElement("li");
+                        listItem.textContent = classId + " - " + classObj.name;
+                        listItem.addEventListener("click", function() {
+                            classInfo(this.innerHTML);
+                        });
+                        classChoice.appendChild(listItem);
+                    }
+                }
+            }
+        }
 }
 
 //This function retrieves the classes taken by the user and
@@ -76,8 +114,7 @@ function userClassesTaken() {
         }
                       
     }
-    console.log(coreTaken);
-    console.log(electiveTaken);
+
 }
 
 function nextSemester() {
@@ -92,41 +129,31 @@ function nextSemester() {
             nextSemesterClass[classes] = elective[classes];
         }
     }
-    console.log(nextSemesterClass);
+    
 }
 
-
+// Logic for class information
 function classInfo(innerHTML) {
-    // Logic for class information
     // Use the `innerHTML` argument to access the inner HTML of the clicked element
-   
     var splitString = innerHTML.split(" - ");
     var courseCode = splitString[0]
-    console.log(courseCode);
+   
     var courseInfo = nextSemesterClass[courseCode];
-    console.log(courseInfo);
+ 
     //populate the class info
     var classInfoHeader= document.getElementById("classInfoHeader");
     var classInfoHTML= document.getElementById("classInfo");
     classInfoHeader.innerHTML = courseCode;
     
-    classInfoHTML.innerHTML = "<b>Course Name: </b>" + courseInfo["name"] + "<br>" +
-                                "<b>Instructor: </b>" + courseInfo["professor"] + "<br>" +
-                                "<b>Semester Available: </b>" + courseInfo["semesterAvailable"] + "<br>";
-    
-  }
 
-function adjustContainerSize() {
-    var listItemHeight = 30; // Adjust the height of each list item in pixels
-    var numItems = nextSemesterClass.children.length;
-    var containerHeight = numItems * listItemHeight;
-    classChoiceContainer.style.height = containerHeight + "px";
-  }
+    classInfoHTML.innerHTML = "<b>Course Name: </b>" + courseInfo["name"] + "<br>" +
+                                "<b>Instructor: </b>" + courseInfo["professor"] + "<br>";
+    
+}
+
 
 function classChecker() {
   nextSemester();
   populateClasses();
-  //adjustContainerSize();
 }
 
-classChecker();
