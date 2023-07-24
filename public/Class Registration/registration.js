61885b2d879c4a31cb444e62e908bdcaec5b9b8b
Sienna <135257695+siennaphia@users.noreply.github.com>
2023-07-24 11:48:45 -0400

    cohesion w code in pages +navbar consistency

--- public/Class Registration/registration.js
+++ public/Class Registration/registration.js
@@ -9,147 +9,45 @@ var firebaseConfig = {
     measurementId: "G-N2V217YPX0"
 };
 
+// initialize Firebase
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 
-//Displays the registration page
-
-//Registration shows all the classes that the user can take
-//  based on the classes they have taken
 let core = {};
 let elective = {};
+let userClasses = {};
 let classesTaken = {};
-let coreTaken = {};
-let electiveTaken = {};
 let nextSemesterClass = {};
 
+
 // Fetch core classes from Firebase
-database.ref('/classes/core').once('value', function(coreSnapshot) {
+database.ref('/classes/core').once('value').then(function(coreSnapshot) {
     core = coreSnapshot.val();
+    console.log(core);
 });
 // Fetch elective classes from Firebase
-database.ref('/classes/elective').once('value', function(electiveSnapshot) {
+database.ref('/classes/elective').once('value').then(function(electiveSnapshot) {
     elective = electiveSnapshot.val();
+    console.log(elective);
 });
 //Fetch classes taken by the current user from Firebase
-database.ref('/users/UID1/classesTaken').once('value', function(classesTakenSnapshot) {
-    classesTaken = classesTakenSnapshot.val();
+database.ref('/users/UID1/classesTaken').once('value').then(function(userClassesSnapshot) {
+    userClasses = userClassesSnapshot.val();
+    userClasses = Object.keys(userClasses);
+    console.log(userClasses);
+    nextSemester(userClasses);
 });
 
-function populateClasses(){
-    
-    var classChoiceContainer = document.getElementById("classChoiceContainer");
-    var classChoice = document.getElementById("classChoice");
-    var dropdownSemester = document.getElementById("semester");
-    var dropdownReason = document.getElementById("reason");
-    
-    
-    var selectedSemester = dropdownSemester.value;
-    var selectedReason = dropdownReason.value;
-    
-    // Clear the classChoice list
-    classChoice.innerHTML = "";
-        // Filter and display classes offered in Fall semester
-        if (selectedSemester == "Fall") {
-            for (var classId in nextSemesterClass) {
+function nextSemester(classesT){
+    // Fetch core classes from Firebase
+    database.ref('/classes/core').once('value').then(function(coreSnapshot) {
+        core = coreSnapshot.val();
+        for(const key in core){
+            if(classesT.includes(core[key]['prerequisites']) || classesT.includes(core[key]['corequisites'])){
+                console.log(core[key]['name']);
+                console.log(core[key]['prerequisites'], core[key]['corequisites']);
                 
-                if (nextSemesterClass.hasOwnProperty(classId)) {        
-                    var classObj = nextSemesterClass[classId];
-                    console.log(classObj["classReason"]);
-                    console.log("==========")
-                    console.log(selectedReason);
-                    
-                    if (classObj["semesterAvailable"] === "Fall" && classObj["classReason"] === selectedReason) {
-                        console.log("==========ASDADADASDASD=========")
-                        var listItem = document.createElement("li");
-                        listItem.textContent = classId + " - " + classObj.name;
-                        listItem.addEventListener("click", function() {
-                            classInfo(this.innerHTML);
-                        });
-                        classChoice.appendChild(listItem);
-                    }
-                }
             }
         }
-        
-        // Filter and display classes offered in Spring semester
-        else if (selectedSemester == "Spring") {
-            for (var classId in nextSemesterClass) {
-                if (nextSemesterClass.hasOwnProperty(classId)) {        
-                    var classObj = nextSemesterClass[classId];
-                    console.log(classObj["classReason"]);
-                    console.log("==========")
-                    console.log(selectedReason);
-                    
-                    if (classObj["semesterAvailable"] === "Spring" && classObj["classReason"] === selectedReason) {
-                        console.log("==========ASDADADASDASD=========")
-                        var listItem = document.createElement("li");
-                        listItem.textContent = classId + " - " + classObj.name;
-                        listItem.addEventListener("click", function() {
-                            classInfo(this.innerHTML);
-                        });
-                        classChoice.appendChild(listItem);
-                    }
-                }
-            }
-        }
+    }); 
 }
-
-//This function retrieves the classes taken by the user and
-//  marks off the classes taken from the core and elective lists.
-//  These are stored in object arrays for further processing.
-function userClassesTaken() {
-    for (const classes in core) {
-        if (classes in classesTaken) {
-            coreTaken[classes] = core[classes];
-        } 
-    }
-    for (const classes in elective) {
-
-        if (classes in classesTaken) {
-            electiveTaken[classes] = elective[classes];
-        }         
-    }
-
-}
-
-function nextSemester() {
-    
-    for (const classes in core) {
-        if (classesTaken[classes] == core[classes]["prerequisites"] || classesTaken[classes] == core[classes]["corequisites"]) {
-            nextSemesterClass[classes] = core[classes];
-        }
-    }
-    for (const classes in elective) {
-        if (classesTaken[classes] == elective[classes]["prerequisites"] || classesTaken[classes] == elective[classes]["corequisites"]) {
-            nextSemesterClass[classes] = elective[classes];
-        }
-    }
-    
-}
-
-// Logic for class information
-function classInfo(innerHTML) {
-    // Use the `innerHTML` argument to access the inner HTML of the clicked element
-    var splitString = innerHTML.split(" - ");
-    var courseCode = splitString[0]
-   
-    var courseInfo = nextSemesterClass[courseCode];
- 
-    //populate the class info
-    var classInfoHeader= document.getElementById("classInfoHeader");
-    var classInfoHTML= document.getElementById("classInfo");
-    classInfoHeader.innerHTML = courseCode;
-    
-
-    classInfoHTML.innerHTML = "<b>Course Name: </b>" + courseInfo["name"] + "<br>" +
-                                "<b>Instructor: </b>" + courseInfo["professor"] + "<br>";
-    
-}
-
-
-function classChecker() {
-  nextSemester();
-  populateClasses();
-}
-
--- public/Login Page/index.js
+++ public/Login Page/index.js
@@ -31,7 +31,7 @@ var auth = firebase.auth();
             // Add other user data as needed
           });
           alert("Registration successful!");
-          window.location.href = "userinfo.html"; // Redirect to userinfo.html
+          window.location.href = "../Profile/userinfo.html"; // Redirect to userinfo.html
         })
         .catch((error) => {
           var errorCode = error.code;
@@ -47,33 +47,10 @@ var auth = firebase.auth();
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       // User is signed in, redirect to userinfo.html or any other authorized page
-      window.location.href = "userinfo.html";
+      window.location.href = "../Profile/userinfo.html";
     }
   });
-  /*
-  document.getElementById("loginForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-    var email = document.getElementById("emailInput").value;
-    var password = document.getElementById("passwordInput").value;
-  
-    if (email && password) {
-      firebase
-        .auth()
-        .signInWithEmailAndPassword(email, password)
-        .then((userCredential) => {
-          alert("Login successful!");
-          window.location.href = "userinfo.html"; // Redirect to userinfo.html
-        })
-        .catch((error) => {
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          alert("Login failed: " + errorMessage);
-        });
-    } else {
-      alert("Please enter a valid email and password.");
-    }
-  });
-*/
+
 function login(){
   var email = document.getElementById("emailInput").value;
   var password = document.getElementById("passwordInput").value;
@@ -84,7 +61,7 @@ function login(){
       .signInWithEmailAndPassword(email, password)
       .then((userCredential) => {
         alert("Login successful!");
-        window.location.href = "userinfo.html"; // Redirect to userinfo.html
+        window.location.href = "../Profile/userinfo.html"; // Redirect to userinfo.html
       })
       .catch((error) => {
         var errorCode = error.code;
@@ -96,4 +73,3 @@ function login(){
   }
 
 }
-  
